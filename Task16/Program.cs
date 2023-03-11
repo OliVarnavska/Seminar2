9786d05b9a761a26a0651639224b1af8f62fc3e9
Oli <oli.varnavska@gmail.com>
2023-03-11 22:15:14 +0800

    Add Task 16

---
+++ Task16/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine($"Введите два числа:");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine()); 
+
+if (( x == Math.Pow(y, 2)) || (y == Math.Pow(x, 2)))
+    Console.WriteLine($"Да");
+else
+    Console.WriteLine($"Нет");
